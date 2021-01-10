e0f0c3c2636d4d1ee49cf8356e01aa16a5fee683
Shahzeb Ahmed <shahzeb2000786@gmail.com>
2021-01-09 23:00:55 -0500

    Added scroll view to stockstat view controller

--- SimStock/Controllers/StockStatsViewController.swift
+++ SimStock/Controllers/StockStatsViewController.swift
@@ -7,18 +7,28 @@
 
 import Foundation
 import UIKit
-class StockStatsViewController: UIViewController{
+class StockStatsViewController: UIViewController, UINavigationControllerDelegate{
     
     weak var stockCurrentPriceLabel: UILabel!
     weak var stockStatView: StockStatView!
     
     var selectedStockTicker: String = "WMT" //{
     
+    var selectedStockArray: [Stock] = []{
+        willSet{
+            DispatchQueue.main.async{
+//                for stock in self.selectedStockArray{
+//                    print(stock.date)
+//                }
+            }
+        }
+    }
     var selectedStock: Stock = Stock(open: "0", high: "0", low: "0", price: "0" , volume: "0", date: "0", previousClose: "0", change: "0", changePercent: "0", exchange: "0", sector: "0", industry: "0", fiftyTwoHigh: "0", fiftyTwoLow: "0", peRatio: "0", marketCap: "0", dividendYield: "0", fiftyDayMovingAverage: "0", description: "0"){
         willSet{
-            DispatchQueue.main.async {
+            print(selectedStock)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5, execute: {
                 self.stockCurrentPriceLabel?.text = self.selectedStock.price
-                self.stockStatView.openPriceLabel.text = self.selectedStock.price
+                self.stockStatView.openPriceLabel.text = self.selectedStock.open
                 self.stockStatView.highPriceLabel.text = self.selectedStock.high
                 self.stockStatView.lowPriceLabel.text = self.selectedStock.low
                 self.stockStatView.fiftyTwoWeekHighLabel.text = self.selectedStock.fiftyTwoHigh
@@ -29,30 +39,32 @@ class StockStatsViewController: UIViewController{
                 self.stockStatView.marketCapLabel.text = self.selectedStock.marketCap
                 self.stockStatView.fiftyDayMovingAverageLabel.text = self.selectedStock.fiftyDayMovingAverage
 
-            }
+            })
         }
         didSet{
-            
+
         }
     }
     
     override func loadView(){
         super.loadView()
-        
+        self.edgesForExtendedLayout = []//makes items which are put into the view appear under the navigation bar
+
         //instantiation of ui elements
         self.view.backgroundColor = UIColor.black
         let bottomBar = Bundle.main.loadNibNamed("BottomBar", owner: nil, options: nil)?.first as! BottomBar//UINib(nibName: "BottomBar", bundle: Bundle.main) as! BottomBar
-        
+
+        let stockScrollView = UIScrollView()
+        stockScrollView.showsHorizontalScrollIndicator = false
+        stockScrollView.contentSize = CGSize(width: self.view.frame.width, height: 2000)
+
         let stockStatView = Bundle.main.loadNibNamed("StockStatView", owner: nil, options: nil)?.first as! StockStatView
         let stockCurrentPriceLabel = UILabel()
-               
-        
-        //adding ui elements to main view
+//
+//        //adding ui elements to main view
         self.view.addSubview(bottomBar)
-        self.view.addSubview(stockStatView)
-        self.view.addSubview(stockCurrentPriceLabel)
-               
-               
+        self.view.addSubview(stockScrollView)
+
         //bottomBar
         bottomBar.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
@@ -61,14 +73,25 @@ class StockStatsViewController: UIViewController{
             bottomBar.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
         ])
         
+        stockScrollView.translatesAutoresizingMaskIntoConstraints = false
+        stockScrollView.backgroundColor = UIColor.red
+        stockScrollView.addSubview(stockStatView)
+        stockScrollView.addSubview(stockCurrentPriceLabel)
+        NSLayoutConstraint.activate([
+            stockScrollView.widthAnchor.constraint(equalToConstant: self.view.frame.width),
+            stockScrollView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            stockScrollView.bottomAnchor.constraint(equalTo: bottomBar.topAnchor)
+        ])
+        
         //stockStatView
+//        print("pizza")
+//        print(stockScrollView.bottomAnchor)
         stockStatView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             stockStatView.widthAnchor.constraint(equalToConstant: self.view.frame.width),
-            stockStatView.heightAnchor.constraint(equalToConstant: self.view.frame.height/2),
-            stockStatView.bottomAnchor.constraint(equalTo: bottomBar.topAnchor)
+            stockStatView.heightAnchor.constraint(equalToConstant: self.view.frame.height/1.5),
         ])
-        
+
         //stockCurrentPriceLabel
         stockCurrentPriceLabel.textAlignment = .center
         stockCurrentPriceLabel.font = UIFont(name: stockCurrentPriceLabel.font.fontName, size: 50)
@@ -77,13 +100,16 @@ class StockStatsViewController: UIViewController{
         stockCurrentPriceLabel.backgroundColor = UIColor.clear
         stockCurrentPriceLabel.textColor = UIColor.white
         stockCurrentPriceLabel.translatesAutoresizingMaskIntoConstraints = false
-        stockCurrentPriceLabel.text = selectedStock.price!
+        stockCurrentPriceLabel.text = "Hello there"
+
+       stockCurrentPriceLabel.text = selectedStock.price!
         NSLayoutConstraint.activate([
             stockCurrentPriceLabel.widthAnchor.constraint(equalToConstant: self.view.frame.width/1.5),
             stockCurrentPriceLabel.heightAnchor.constraint(equalToConstant: self.view.frame.height/15),
-            stockCurrentPriceLabel.bottomAnchor.constraint(equalTo: stockStatView.topAnchor),
             stockCurrentPriceLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
         ])
+
+       
         
         
 
@@ -94,7 +120,9 @@ class StockStatsViewController: UIViewController{
     }
     override func viewDidLoad(){
         super.viewDidLoad()
+        navigationController?.delegate = self
         getSelectedStock(ticker: selectedStockTicker)
+        getDailySelectedStock(ticker: selectedStockTicker)
     }
 }
 
@@ -104,7 +132,6 @@ extension StockStatsViewController{
     func getSelectedStock(ticker: String){
         let urlString = "http://localhost:3000/current/" + ticker
         print(urlString)
-        print("The fjsl;fj is " + ticker)
         let url = URL(string: urlString)!
         let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
             if error != nil || data == nil{
@@ -123,14 +150,44 @@ extension StockStatsViewController{
                 let stockData = try decoder.decode(Stock.self, from: data!)
                 print(stockData)
                 print(ticker)
-                self.selectedStock = stockData
+                //self.selectedStock = stockData
             }catch{
                 print ("Error in decoding JSON" + error.localizedDescription)
             }
             
         }//end of task
         task.resume()
-    }//end of getstockData function
+    }//end of getStockData function
+    
+    //function calls daily stock get route which returns an array stock objects from the past 100 days
+    func getDailySelectedStock(ticker: String){
+        let urlString = "http://localhost:3000/daily/" + ticker
+        print(urlString)
+        let url = URL(string: urlString)!
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if error != nil || data == nil{
+                print(error?.localizedDescription)
+                print("There was an error in retreiving information from the Alpha Vantage Api")
+                fatalError("There was an error in retreiving information from the Alpha Vantage Api")
+                
+            }
+            guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode)else{
+                print("Error in server")
+                fatalError("Error in getting response from server")
+            }
+            do{
+                print(data)
+                let decoder = JSONDecoder()
+                let stockData = try decoder.decode([Stock].self, from: data!)
+                print(ticker)
+                self.selectedStockArray = stockData
+            }catch{
+                print ("Error in decoding JSON" + error.localizedDescription)
+            }
+            
+        }//end of task
+        task.resume()
+    }//end of getStockData function
 }
 
 
